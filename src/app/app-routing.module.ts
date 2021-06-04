86380f2c2f3320de45b2aa49d7878d5e55d60405
Eric <Eric@ERICACER-PC>
2021-06-04 16:53:27 -0300

    Subindo grcteam components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { SiteComponent } from './site/site.component';
-import { PoliticaComponent } from './site/_components/politica/politica.component';
-
+import { FooterComponent } from './site/_components/footer/footer.component';
+import { HeaderComponent } from './site/_components/header/header.component';
+import { NavbarComponent } from './site/_components/navbar/navbar.component';
+import { HomeComponent } from './site/_pages/home/home.component';
+import { PoliticaprivacidadeComponent } from './site/_pages/politicaprivacidade/politicaprivacidade.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
-  {path: 'home',          component: SiteComponent},
-  {path: 'politicaprivacidade',      component: PoliticaComponent},
+  {path: 'header',              component: HeaderComponent},
+  {path: 'nav',                 component: NavbarComponent},
+  {path: 'footer',              component: FooterComponent},
+  // {path: 'home',                component: SiteComponent},
+  {path: 'home',                component: HomeComponent},
+  {path: 'politicaprivacidade', component: PoliticaprivacidadeComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SiteComponent } from './site/site.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatIconModule} from '@angular/material/icon';
-import { PoliticaComponent } from './site/_components/politica/politica.component';
-
+import { MatIconModule} from '@angular/material/icon';
+import { MatDividerModule} from '@angular/material/divider';
+import { NavbarComponent } from './site/_components/navbar/navbar.component';
+import { PoliticaprivacidadeComponent } from './site/_pages/politicaprivacidade/politicaprivacidade.component';
+import { HeaderComponent } from './site/_components/header/header.component';
+import { FooterComponent } from './site/_components/footer/footer.component';
+import { HomeComponent } from './site/_pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-      SiteComponent,
-      PoliticaComponent
+    HeaderComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+      
+      PoliticaprivacidadeComponent
    ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatIconModule
+    MatIconModule,
+    MatDividerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
