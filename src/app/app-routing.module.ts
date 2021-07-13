17b0c20ee40e560cc1b98734141ac124db285af2
Eric <Eric@ERICACER-PC>
2021-07-13 13:56:15 -0300

    atualizar grcteam 13/07 13:56

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { FaleconoscoComponent } from './site/_pages/faleconosco/faleconosco.component';
 import { HomeComponent } from './site/_pages/home/home.component';
+import { FaleconoscoComponent } from './site/_pages/faleconosco/faleconosco.component';
 import { PoliticaprivacidadeComponent } from './site/_pages/politicaprivacidade/politicaprivacidade.component';
 
 const routes: Routes = [
   {path: '',                    redirectTo: 'home',                 pathMatch: 'full'},
   {path: 'home',                redirectTo: 'home',                 pathMatch: 'full'},
-  {path: 'politicaprivacidade', redirectTo: 'politicaprivacidade',  pathMatch: 'full'},
   {path: 'faleconosco',         redirectTo: 'faleconosco',          pathMatch: 'full'},
+  {path: 'politicaprivacidade', redirectTo: 'politicaprivacidade',  pathMatch: 'full'},
   {path: 'home',                component: HomeComponent},
-  {path: 'politicaprivacidade', component: PoliticaprivacidadeComponent},
   {path: 'faleconosco',         component: FaleconoscoComponent},
+  {path: 'politicaprivacidade', component: PoliticaprivacidadeComponent},
 ];
 
 @NgModule({
